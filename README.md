42eb7b5402447e0b9c5a0079077468b7763c19a8
Yaroslav Kush <yaroslav_kush@epam.com>
2019-11-08 18:49:12 +0200

    [REFACTOR] : File README.md was changed

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # CreatorOfSuitFile
 
-First of all:
-
-Set your credentials in the configuration file.
+Set your credentials in the "credentials.properties" file.
 
 If you want create excel table of results you need:
 1) Open Run/Debug configuration
 2) Add new "Application"
 3) Enter in the "Main class" - "CreateExcelTabl"
-
-After that you able to create suite file.
 
 If you want create suite file of results you need:
 1) Open Run/Debug configuration
--- src/test/java/CreateExcelTabl.java
+++ src/test/java/CreateExcelTabl.java
@@ -3,8 +3,8 @@ import enums.ReportPages;
 public class CreateExcelTabl {
 
     public static void main(String[] args) {
-//        MainFunctions.createExcelTableWithFailedSku(ReportPages.MOBC_REPORT_PAGE, ReportPages.DTH_REPORT_PAGE, ReportPages.DTH_SINGLE_RUN_REPORT_PAGE,
-//                ReportPages.DTH_VPOS_OFF_REPORT_PAGE, ReportPages.CNC_REPORT_PAGE, ReportPages.DTH_SINGLE_RUN_REPORT_PAGE, ReportPages.ISSO_REPORT_PAGE);
-        MainFunctions.createExcelTableWithFailedSku(ReportPages.CIAM_REPORT_PAGE,ReportPages.ISSO_REPORT_PAGE);
+        MainFunctions.createExcelTableWithFailedSku(ReportPages.MOBC_REPORT_PAGE, ReportPages.DTH_REPORT_PAGE, ReportPages.DTH_SINGLE_RUN_REPORT_PAGE,
+                ReportPages.DTH_VPOS_OFF_REPORT_PAGE, ReportPages.CNC_REPORT_PAGE, ReportPages.DTH_SINGLE_RUN_REPORT_PAGE, ReportPages.ISSO_REPORT_PAGE);
+//        MainFunctions.createExcelTableWithFailedSku(ReportPages.CIAM_REPORT_PAGE,ReportPages.ISSO_REPORT_PAGE);
     }
 }
